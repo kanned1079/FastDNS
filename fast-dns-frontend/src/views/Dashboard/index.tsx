80a56288a41b2f889585ba347acc4cdfc2a4109f
kanned1079 <kanned1079@gmail.com>
2025-03-18 12:16:59 +0800

    Update Mar_18

--- fast-dns-frontend/src/views/Dashboard/index.tsx
+++ fast-dns-frontend/src/views/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import instance from "../../axios/index"
-import {useEffect, useState} from "react";
+import {useEffect, useState, useMemo} from "react";
 import {Card} from "antd"
 import PageHead from "../PageHead"
 import {type Response} from "../../types.tsx";
@@ -19,33 +19,52 @@ function Dashboard() {
         }
     }
     // fetchDataClick()
+    let intervalId: number|undefined = undefined
+    const startLookup = () => {
+        intervalId = setInterval(() => fetchDataClick(), 1000)
+    }
+
+    const reqAll = useMemo(() => {
+        if (data?.cache.cache_miss === undefined || data?.cache.cache_hint === undefined)
+            return 0
+        return data?.cache.cache_miss + data?.cache.cache_hint
+    }, [data?.cache.cache_miss, data?.cache.cache_hint])
 
     const topCardContent = [
         {
             title: 'DNS缓存命中率',
             data: data?.cache.cache_rate,
             type: 'rate',
-            description: '如果缓存中已有该域名的解析记录，可以直接返回该结果，避免再次向上游 DNS 服务器发起请求。缓存命中率（hit rate）就是命中请求占所有 DNS 请求的比例。',
+            description: '如果缓存中已有该域名的解析记录，可以直接返回该结果，避免再次向上游 DNS 服务器发起请求。',
         },
         {
             title: 'DNS缓存命中次数',
             data: data?.cache.cache_hint,
             type: 'number',
-            description: '具体来说，缓存提示（hint）通常表示该记录在缓存中，但它可能存在一些过期、失效或者需要再次验证的风险。它是为了提升性能和减少查询延迟而引入的一种策略。',
+            description: '具体来说，缓存提示（hint）通常表示该记录在缓存中，但它可能存在一些过期、失效或者需要再次验证的风险。',
 
         },
         {
             title: '没有找到缓存',
             data: data?.cache.cache_miss,
             type: 'number',
-            description: 'Cache Miss 发生在系统无法从缓存中获取到所请求的域名解析记录时。此时，DNS 系统会向上游的 DNS 服务器发起请求，从而获取新的解析结果。未命中的情况会导致查询延迟增加。',
-
+            description: '发生在系统无法从缓存中获取到所请求的域名解析记录时。此时，DNS 系统会向上游的 DNS 服务器发起请求，从而获取新的解析结果。',
+        },
+        {
+            title: '请求总计',
+            data: reqAll,
+            type: 'number',
+            description: '包含成功和失败相应的计数。',
         },
     ]
 
     useEffect(() => {
         console.log('dashboard mount')
         fetchDataClick()
+        startLookup()
+        return () => {
+            intervalId?clearInterval(intervalId):null
+        }
     }, []);
 
     return (
@@ -72,7 +91,7 @@ function Dashboard() {
                             ? (item.data * 100).toFixed(2) + " %"
                             : item.data + ' 次'
                         }</p>
-                        <p style={{marginTop: '10px', opacity: '0.8'} as React.CSSProperties}>{item.description}</p>
+                        {/*<p style={{marginTop: '10px', opacity: '0.8'} as React.CSSProperties}>{item.description}</p>*/}
                     </Card>
                 ))}
             </div>
@@ -81,9 +100,6 @@ function Dashboard() {
                 description={'在这里将显示具体的配置信息，如使用的DNS服务器主从列表、是否启用EDNS和SCDNS等高级应用'}
             ></PageHead>
 
-            <>
-
-            </>
 
 
         </div>
